8dc32340853b8000871a5941f5f6271b71e1cbde
Lokeswari3173 <lokipstla@gmail.com>
2026-06-27 15:15:45 +0530

    Final commit

--- utils/ai_analyzer.py
+++ utils/ai_analyzer.py
@@ -3,10 +3,10 @@ import os
 from dotenv import load_dotenv
 import json
 import re
-
+import streamlit as st
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=st.secrets("GOOGLE_API_KEY"))
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
